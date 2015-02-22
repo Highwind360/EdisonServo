70c2a3e9528ffada3361198234fb53e1193e5ded
Grayson Sinclair <highwind360@gmail.com>
2015-02-22 05:12:19 -0800

    Upgraded Dan's otherserver.js

--- otherserver.js
+++ otherserver.js
@@ -11,15 +11,28 @@ Cylon.robot({
 
   devices: {
     servo: { driver: "servo", pin: servoPin },
-    sensor: { driver: "analogSensor", pin: analogPin, lowerLimit: 0, upperLimit:180}
+    sensor: { driver: "analogSensor", pin: analogPin, lowerLimit: 0, upperLimit:1024}
   },
 
   work: function(my) {
-    var angle = 0;
+    var angle = 0,
+    prevAngle = angle,
+    updateCap = 0,
+    beenCalm = 0;
 
     every((0.30).second(), function() {
-      angle = my.sensor.analogRead();
-      my.servo.angle(angle);
+      angle = Math.floor(my.sensor.analogRead() / 57) * 10;
+      console.log('%d', angle);
+      if (angle != prevAngle && updateCap < 10) {
+        my.servo.angle(angle);
+        prevAngle = angle;
+      } else {
+	beenCalm++;
+	if (beenCalm > 4) {
+	  updateCap = 0;
+	  beenCalm = 0;
+	}
+      }
     });
   }
 }).start();
