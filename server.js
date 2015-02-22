13cde772b8edfb123cf20efa9b3b7130126e8d2f
Grayson Sinclair <highwind360@gmail.com>
2015-02-22 04:05:31 -0800

    The server now interacts with a button

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var Cylon = require("cylon");
+var servoPin = 3;
+var buttonPin = 2;
 
 Cylon.robot({
   connections: {
@@ -8,23 +10,30 @@ Cylon.robot({
   },
 
   devices: {
-    servo: { driver: "servo", pin: 3, }
+    servo: { driver: "servo", pin: servoPin }
+    button: { driver: "button", pin: buttonPin }
   },
 
   work: function(my) {
     var angle = 0,
-    increment = 20;
+    increment = 20,
+    upperLim = 160;
 
     every((1).second(), function() {
       angle += increment;
 
+      if ((angle % upperLim === 0)) {
+        increment -= increment;
+      }
+
       my.servo.angle(angle);
 
-      console.log("Current Angle: " + (my.servo.currentAngle()));
-
-      if ((angle === 0) || (angle === 180)) {
+      my.button.on('release', function() {
         increment = -increment;
-      }
+        console.log('Flipping around!')
+      });
+
+      console.log("Current Angle: " + (my.servo.currentAngle()));
     });
   }
 }).start();
