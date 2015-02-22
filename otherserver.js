92961820cb673b9622a911a6d52358dc4a9d13da
DMArens <darens103@gmail.com>
2015-02-22 03:21:31 -0800

    wrote my attempt

--- otherserver.js
+++ otherserver.js
@@ -1,2 +1,25 @@
-test
+var Cylon = require('cylon');
+/*
+var net = require('net');
+var io = require('socket.io').listen(1337);
+*/
 
+Cylon.robot({
+  devices: {
+    servo: {driver: 'servo', pin: 3, connection:'edison'}
+  },
+
+  work: function(my) {
+    var agnle = 45;
+    my.servo.angle(angle);
+    every((1).second(), function() {
+      angle = angle + 45;
+      if (angle > 135) {
+        angle = 45
+      }
+      my.servo.angle(angle);
+    });
+  }
+})
+
+Cylon.start();
